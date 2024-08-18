09bd8813ec5891f382f4ab86bdceecd2be56454a
Jaime <garciadiazjaime@gmail.com>
2024-08-18 12:22:18 -0500

    feat: ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -11,6 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="UA-46136944-1" />
     </html>
   );
 }
